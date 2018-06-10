90b256cd06b4d10d46ae50634e1fedb62873fffa
klimas7 <klimas7@gmail.com>
2018-06-10 16:26:02 +0200

    Add parameter Value

--- src/main/java/pl/sii/calculatorPlugin/CalculatorParameterDefinition.java
+++ src/main/java/pl/sii/calculatorPlugin/CalculatorParameterDefinition.java
@@ -1,4 +1,77 @@
 package pl.sii.calculatorPlugin;
 
-public class CalculatorParameterDefinition {
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import javax.annotation.CheckForNull;
+
+import hudson.Extension;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParameterValue;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+public class CalculatorParameterDefinition extends ParameterDefinition {
+
+    @DataBoundConstructor
+    public CalculatorParameterDefinition(String name, String description) {
+        super(name, description);
+    }
+
+    @CheckForNull
+    @Override
+    public ParameterValue createValue(StaplerRequest req, JSONObject jsonObject) {
+        String name = getName();
+        CalculatorParameterValue calculatorParameterValue = new CalculatorParameterValue(name, getDescription());
+        calculatorParameterValue.setFirst(req.getParameter(name + "_first"));
+        calculatorParameterValue.setSecond(req.getParameter(name + "_second"));
+        calculatorParameterValue.setOperation(Operation.valueOf(req.getParameter(name + "_operation")));
+        return calculatorParameterValue;
+    }
+
+    @CheckForNull
+    @Override
+    public ParameterValue createValue(StaplerRequest staplerRequest) {
+        CalculatorParameterValue calculatorParameterValue = new CalculatorParameterValue(getName(), getDescription(), "0", "0", Operation.ADD);
+        return calculatorParameterValue;
+    }
+
+    public Operation[] getOperations() {
+        return Operation.values();
+    }
+
+    public String getRootUrl() {
+        return Jenkins.getInstance().getRootUrl();
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl) super.getDescriptor();
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends ParameterDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Calculator parameter";
+        }
+
+        /**
+         * Called to validate the passed user entered value against the configured regular expression.
+         */
+        public FormValidation doValidate(@QueryParameter("value") final String value) {
+            try {
+                if (Pattern.matches("\\d*", value)) {
+                    return FormValidation.ok();
+                } else {
+                    return FormValidation.error("Należy podać liczbę całkowitą");
+                }
+            } catch (PatternSyntaxException pse) {
+                return FormValidation.error("Unexpected error: " + pse.getDescription());
+            }
+        }
+    }
 }
--- src/main/java/pl/sii/calculatorPlugin/CalculatorParameterValue.java
+++ src/main/java/pl/sii/calculatorPlugin/CalculatorParameterValue.java
@@ -1,5 +1,52 @@
 package pl.sii.calculatorPlugin;
 
-public class CalculatorParameterValue {
+import hudson.model.ParameterValue;
+import org.kohsuke.stapler.DataBoundConstructor;
 
+public class CalculatorParameterValue extends ParameterValue {
+    private String first;
+    private String second;
+    private Operation operation;
+
+    @DataBoundConstructor
+    public CalculatorParameterValue(String name, String description) {
+        this(name, description, null, null, Operation.ADD);
+    }
+
+    public CalculatorParameterValue(String name, String description, String first, String second, Operation operation) {
+        super(name, description);
+        this.first = first;
+        this.second = second;
+        this.operation = operation;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String toString() {
+        return "(CalculatorParameterValue) " + this.getName() + ": First: " + this.first + " Second: " + this.second;
+    }
+
+    //TODO createBuildWrapper
 }
